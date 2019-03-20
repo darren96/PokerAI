66a4af0cc5209df0d7a095fcf97df0b2aafdf91f
AZhiKai <ang-zhikai@outlook.com>
2019-03-20 14:31:21 +0800

    raise_player: Add output of action history
    This'd allow us to debug easier, as the game verbose output might not be sufficient to help us understand the gameplay.

--- example.py
+++ example.py
@@ -12,3 +12,9 @@ config.register_player(name="FT2", algorithm=RaisedPlayer())
 
 
 game_result = start_poker(config, verbose=1)
+
+# verbose=0 because game progress is visualized by ConsolePlayer
+# verbose=1 allows simple game logs to output after start_poker call
+"""
+If wish to participate in the game as a console player, check https://ishikota.github.io/PyPokerEngine/tutorial/participate_in_the_game/
+"""
